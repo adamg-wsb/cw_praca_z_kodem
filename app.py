e508cf4b4cc362b3514ab17d2f3376cfbce069cd
unknown <wrx71429@student.wsb.wroclaw.pl>
2022-04-03 22:04:28 +0200

    zad 3

---
+++ app.py
@@ -0,0 +1,12 @@
+""" docsting """
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    """docstring"""
+    return '<h1>Hello WSB! Greetings from Flask & Docker!</h1>'
+
+if __name__ == "__main__":
+    app.run(debug=True)
